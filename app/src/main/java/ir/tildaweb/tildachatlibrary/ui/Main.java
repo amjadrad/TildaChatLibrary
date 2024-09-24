5f0510e0591a3b78275c5510e01c3134d5a866d4
Amjad Qasemie Rad <amjadrad@users.noreply.github.com>
2024-09-24 15:01:02 +0330

    Init 2

---
+++ app/src/main/java/ir/tildaweb/tildachatlibrary/ui/Main.java
@@ -0,0 +1,17 @@
+package ir.tildaweb.tildachatlibrary.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import ir.tildaweb.tildamessaging.JustText;
+
+public class Main extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        JustText.TestMe("l");
+    }
+}
---
+++ tildamessaging/src/main/java/ir/tildaweb/tildamessaging/JustText.java
@@ -0,0 +1,10 @@
+package ir.tildaweb.tildamessaging;
+
+import android.util.Log;
+
+public class JustText {
+
+    public static void TestMe(String s) {
+        Log.d("JustTest", "TestMe: " + s);
+    }
+}
